aad21fb26a058f4081cbfee41c05163b8440d6d1
unknown <wendywendo06@gmail.com>
2024-10-08 18:10:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  var [characters, setCharacters] = useState(0);
+  var [sentences, setSentences] = useState("");
+  var [words, setWords] = useState(0);
+
+  useEffect(() => {
+    setCharacters(sentences.length);
+
+    if (sentences.length > 0) {
+      const array = sentences.trim().split(/\s+/);
+      setWords(array.length);
+    }
+  }, [sentences, words]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Word Counter</h1>
+      <p className="tagName">Free online character and word count tool.</p>
+
+      <textarea
+        rows={ 9 }
+        placeholder="Type or paste your text"
+        value={sentences}
+        onChange={(e) => setSentences(e.target.value)}
+      ></textarea>
+
+      <button 
+        className="clearBtn" 
+        disabled={sentences.length <= 0}
+        onClick={() => {
+          setSentences("");
+          setWords(0);
+        }}
+      >Clear</button>
+
+      <div className="results">
+        <p>Character: { characters }</p>
+        <p>Word: { words }</p>
+      </div>
+
+    </main>
   );
 }
 
